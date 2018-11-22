8d0c31f295cadf26f4faff04022a533a32bd199a
ThomasAunvik <thaun1@live.no>
2018-11-22 21:23:11 +0100

    Initial commit

---
+++ Handler/ServerUser.cs
@@ -0,0 +1,39 @@
+﻿using Discord.WebSocket;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hifumi_Bot
+{
+    [Serializable]
+    public class ServerUser
+    {
+        public string username;
+        public ulong userID;
+        public bool isBot;
+
+        public ServerUser(SocketGuildUser user)
+        {
+            if(user != null)
+            {
+                username = user.Username;
+                userID = user.Id;
+                isBot = user.IsBot;
+            }
+        }
+
+        public void UpdateInfo(SocketGuildUser user)
+        {
+            if(user != null)
+            {
+                username = user.Username;
+                userID = user.Id;
+                isBot = user.IsBot;
+            }
+        }
+    }
+}
